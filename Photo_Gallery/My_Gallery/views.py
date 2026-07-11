daa6558986ae3adc4451835505ddb4c979d5e52a
derick871 <wanyamaderrick4@gmail.com>
2026-07-11 12:16:36 +0300

    update login and urls file

--- Photo_Gallery/My_Gallery/views.py
+++ Photo_Gallery/My_Gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    return render(request, 'register.html')
