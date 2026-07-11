b263acca7d8c375ec4d1e2c651daf45de3fb617b
derick871 <wanyamaderrick4@gmail.com>
2026-07-11 19:29:53 +0300

    modify index and the app's urls file

--- Photo_Gallery/My_Gallery/urls.py
+++ Photo_Gallery/My_Gallery/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
+    path('', views.index, name= "index"),
+    path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('profile/', views.profile, name= "profile"),
+    path('photo_details' views.photo_details, name= "photo_details"),
+
 ]
